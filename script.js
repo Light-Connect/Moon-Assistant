55d077775baba07f749c1583c641b3e40c911876
lightconnect <83376673+lightconnect@users.noreply.github.com>
2021-04-28 20:24:09 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition; 
+var recognition = new SpeechRecognition(); // This runs when the speech recognition service starts 
+recognition.onstart = function() { 
+console.log("We are listening. Try speaking into the microphone."); 
+}; 
+recognition.onspeechend = function() { 
+recognition.stop(); 
+}
+ recognition.onresult = function(event) { 
+var transcript = event.results[0][0].transcript; 
+var confidence = event.results[0][0].confidence;
+document.getElementById("you").textContent = transcript; 
+}; // start recognition 
+recognition.start();
